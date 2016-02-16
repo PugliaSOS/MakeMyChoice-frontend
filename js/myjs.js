620c2515bf1d135592a9e397240b76d02d876307
MessineseGianluca <gianluca.messi1@gmail.com>
2016-02-16 20:06:32 +0100

    Create screen opacity when click search-bar

--- js/myjs.js
+++ js/myjs.js
@@ -1,3 +1,11 @@
 $(".search_input").click(function() {
   $(".search_input").val("");
+  $("body").css("background-color", "#808080");
+  $(".container-fluid").css("opacity", "0.3");
+});
+
+$(".container-fluid").click(function() {
+  $(".search_input").val("MakeMyChoice");
+  $("body").css("background-color", "#FFFFFF");
+  $(".container-fluid").css("opacity", "1");
 });
