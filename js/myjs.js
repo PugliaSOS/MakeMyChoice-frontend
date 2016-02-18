e388dc69bbece54f8cd587d5c68d01768a95c844
MessineseGianluca <gianluca.messi1@gmail.com>
2016-02-18 20:33:52 +0100

    Some design fixes

--- js/myjs.js
+++ js/myjs.js
@@ -3,14 +3,12 @@ $( document ).ready(function() {
   $(".search").click(function() {
 	$(".title").addClass("hidden");
   	$(".search_input").removeClass("hidden");
-  	$("body").css("background-color", "#808080");
   	$(".container-fluid").css("opacity", "0.3");
   });
 
   $(".container-fluid").click(function() {
     $(".search_input").addClass("hidden");
     $(".title").removeClass("hidden")
-  	$("body").css("background-color", "#FFFFFF");
   	$(".container-fluid").css("opacity", "1");
   });
 });
