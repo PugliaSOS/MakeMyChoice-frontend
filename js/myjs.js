7ee722070db85c8443323609fca0d27141b9ef24
MessineseGianluca <gianluca.messi1@gmail.com>
2016-02-19 20:53:56 +0100

    Fix navbar style

--- js/myjs.js
+++ js/myjs.js
@@ -7,13 +7,7 @@ $( document ).ready(function() {
 
   $(".search").click(function() {
 	$(".title").addClass("hidden");
-  	$(".search_input").removeClass("hidden");
-  	$(".container-fluid").css("opacity", "0.3");
-  });
-
-  $(".container-fluid").click(function() {
-    $(".search_input").addClass("hidden");
-    $(".title").removeClass("hidden")
-  	$(".container-fluid").css("opacity", "1");
+  	$(".search_bar").removeClass("hidden");
+  	$(".overlay").css("opacity", "0.3");
   });
 });
