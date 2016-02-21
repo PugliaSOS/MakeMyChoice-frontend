73f608079db03bbafd15ec66fcd9229443569d31
Dilillo Nicola <nicoladilillo98@gmail.com>
2016-02-21 16:15:10 +0100

    Start fix css autocomplete

--- js/myjs.js
+++ js/myjs.js
@@ -1,10 +1,11 @@
 $( document ).ready(function() {
 
-  $(".search_input").autocomplete({
+  $(".search_input").autocomplete({ 
     source: ['Tv','Cellulari','Computer','Tablet','TV','T-Shirt'],
-    minLength:1
-  });
-
+    minLength:1,
+    position: { of :"#search_bar" }
+  }); 
+ 
   $(".search").click(function() {
 	$(".title").addClass("hidden");
   	$(".search_bar").removeClass("hidden");
