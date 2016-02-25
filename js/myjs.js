5c3d9998e75a884397153951e2929ed40336ed8f
Dilillo Nicola <nicoladilillo98@gmail.com>
2016-02-25 19:42:36 +0100

    Add suggestion

--- js/myjs.js
+++ js/myjs.js
@@ -1,14 +1,22 @@
 $( document ).ready(function() {
 
   $(".search_input").autocomplete({ 
-    appendTo: "overlay",
+    appendTo: ".overlay",
     source: ['Tv','Cellulari','Computer','Tablet','TV','T-Shirt'],
     minLength:1,
   }); 
- 
+
+  $(".search_input").keyup(function() {
+    $(".autocomplete-new").remove();
+    var str = $( ".search_input" ).val();
+    $(".overlay").append("<ul class='ui-autocomplete ui-menu autocomplete-new'><li><span class='icon-plus'>+</span>"+str+"</li></ul>");
+
+  });
+  
   $(".search").click(function() {
-	$(".title").addClass("hidden");
+	  $(".title").addClass("hidden");
   	$(".search_bar").removeClass("hidden");
   	$(".overlay").css("background-color", "rgba(128, 128, 128, 0.5)");
   });
+
 });
